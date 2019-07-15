7e259a3650591ec9113215b11543be6263192975
kazuki <sato.kazuki@lancerbiz.com>
2019-07-15 15:17:27 +0700

    カードでの商品一覧仮作成

---
+++ app/javascript/components/top/MainContent.js
@@ -0,0 +1,37 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class MainContent extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Card />
+      </React.Fragment>
+    );
+  }
+}
+
+function Card() {
+  return(
+    <div className="row">
+      <div className="col s6 m9">
+        <h2 className="header">Recommandation</h2>
+        <a href="#">
+          <div className="card horizontal">
+            <div className="card-image">
+              <img src="https://lorempixel.com/100/190/nature/6" />
+            </div>
+            <div className="card-stacked">
+              <div className="card-content">
+                <p>Hatomugi genmai</p>
+                <p>2000円</p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default MainContent
